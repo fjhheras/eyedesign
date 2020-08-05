610fc787bb1012b25f21dd72ddced90ca4e8caef
F.J.H. Heras <fjhheras@gmail.com>
2020-08-05 12:32:41 +0100

    Updating simple to new c

--- R_in_simple.m
+++ R_in_simple.m
@@ -1,17 +1,18 @@
 function [ R ] = R_in_simple( V,L,d,c,N,V0 )
 %R_IN_SIMPLE Calculate R in a simple eye, given
-%   V - Volume
+%   V - Volume equivalent of investment
 %   L - Ph length
-%   d - Ph width
+%   d - Distance between ph centers (normally Ph width)
 %   c - cost
 %   N - Number tr units per ph
-%   V0 - Volume in Calliphora
 
   K1 = V / (4*pi/3);
-  old_inv = V / V0;
+  %old_inv = V / V0;
   %n3=1.35;
   syms r;
-  R=vpasolve(K1 - (r+L)^3 - K1/old_inv*c*4*pi/d/d/(2*sqrt(3)/3)*r*r*N);
+  % Num photoreceptors: 4*pi*r*r/d/d/(2*sqrt(3)/3)
+  % Investment (K1) must be equal to 
+  R = vpasolve( K1 - (r+L)^3 - c*N*(4*pi*r*r/d/d/(2*sqrt(3)/3)) / (4*pi/3) );
   %And we keep the only positive real root
   R = eval(R);
   R = R(imag(R)==0);
